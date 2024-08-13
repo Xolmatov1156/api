fe74e62fc7cc1c3dbb157d0a212f07b631cd6839
Xolmatov1156 <xolmatov1156@gmail.com>
2024-08-13 20:36:44 +0500

    api

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,46 @@
-import './App.css'
-import Shop from './pages/Shop'
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Shop from "./pages/Shop";
+import "./App.css";
+import { ThreeDots } from 'react-loader-spinner';
 
 function App() {
-
-  return (
-    <>
-     <Shop/>
-    </>
-  )
+    const About = lazy(
+        () =>
+            new Promise((resolve) => {
+                return setTimeout(() => resolve(import("./pages/About")), 1500);
+            })
+    );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Shop />} />
+                <Route
+                    path="/about"
+                    element={
+                        <Suspense
+                            fallback={
+                                <div className="w-screen h-screen flex items-center justify-center">
+                                    <ThreeDots
+                                        visible={true}
+                                        height="80"
+                                        width="80"
+                                        color="#4fa94d"
+                                        radius="9"
+                                        ariaLabel="three-dots-loading"
+                                        wrapperStyle={{}}
+                                        wrapperClass=""
+                                    />
+                                </div>
+                            }
+                        >
+                            <About />
+                        </Suspense>
+                    }
+                ></Route>
+            </Routes>
+        </Router>
+    );
 }
 
-export default App
+export default App;
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,28 +1,72 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ThreeDots } from "react-loader-spinner";
+import Basket from "../assets/images/basket.svg";
+import toast, { Toaster } from "react-hot-toast";
 
 function Shop() {
     const [users, setUsers] = useState([]);
+    const navigate = useNavigate();
 
     useEffect(() => {
         fetch("https://api.escuelajs.co/api/v1/products")
-            .then(res => res.json())
-            .then(data => {
+            .then((res) => res.json())
+            .then((data) => {
                 setTimeout(() => {
                     setUsers(data);
                 }, 1000);
             });
     }, []);
 
+    const handleItemClick = (item) => {
+        navigate("/about", { state: { item } });
+    };
+
     return (
-        <ul className='flex space-x-[20px] flex-wrap mt-[50px] justify-center space-y-[20px] pb-[20px]'>
-            {
-                users.length ? users.slice(1).splice(1, 35).map(item => (
-                    <li key={item.id} className='w-[300px] bg-white h-[350px] border-[1px] rounded-lg border-slate-400 p-[10px]'>
-                        <p className='font-semibold text-center'>{item.title}</p>
-                        <img src={item.images[0]} alt="Product" className='rounded-lg mx-auto mt-[10px] max-w-[250px] max-h-[250px]'/>
+        <ul className="flex space-x-[20px] flex-wrap mt-[50px] w-[1600px] mx-auto space-y-[20px] pb-[40px]">
+            {users.length ? (
+                users.slice(0, 34).map((item) => (
+                    <li
+                        key={item.id}
+                        className="w-[300px] shadow text-white h-[400px] mt-[20px] ml-[20px] border-[1px] flex flex-col justify-between items-center rounded-lg border-white p-[10px] cursor-pointer"
+                        
+                    >
+                        <Toaster position="top-center" reverseOrder={false} />
+                        <p className="font-semibold text-center">
+                            {item.title}
+                        </p>
+                        <img
+                            src={item.images[0]}
+                            width={250}
+                            height={250}
+                            alt="Product"
+                            className="rounded-lg mx-auto mt-[10px]"
+                            onClick={() => handleItemClick(item)}
+                        />
+                        <div className="flex gap-[50px] items-center pb-[7px]">
+                            <span className="border-[1px] border-white p-1 rounded-lg">
+                                {item.price}$
+                            </span>
+                            <button onClick={() => {toast.success("Sotib olindi")}} className="border-[1px] border-white p-1 rounded-lg">
+                                <img src={Basket} alt="basket" className="" />
+                            </button>
+                        </div>
                     </li>
-                )) : "Malumot yo'q"
-            }
+                ))
+            ) : (
+                <div className="w-screen h-screen flex items-center justify-center">
+                    <ThreeDots
+                        visible={true}
+                        height="80"
+                        width="80"
+                        color="#4fa94d"
+                        radius="9"
+                        ariaLabel="three-dots-loading"
+                        wrapperStyle={{}}
+                        wrapperClass=""
+                    />
+                </div>
+            )}
         </ul>
     );
 }
